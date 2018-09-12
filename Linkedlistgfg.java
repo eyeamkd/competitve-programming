c816d25dd5c41a05654fa06ca1ac686301f171bb
Kunal Dubey <kunaldubey2297@gmail.com>
2018-09-13 05:25:10 +0530

    gfg linked list

---
+++ Linkedlistgfg.java
@@ -0,0 +1,42 @@
+import java.util.*; 
+import java.io.*; 
+class Linkedlistgfg { 
+	public static void main(String[] args) {  
+		Scanner ob=new Scanner(System.in);
+		LinkedList<Integer> original_list=new LinkedList<Integer>(); 
+		LinkedList<Integer> even_list = new LinkedList<Integer>(); 
+		LinkedList<Integer> odd_list = new LinkedList<Integer>();
+		LinkedList<Integer> final_list = new LinkedList<Integer>();  
+		 
+		int flag=0; 
+		while(flag==0) {  
+			System.out.println("1. Enter Element \n 2.Exit "); 
+			int choice=ob.nextInt(); 
+			if(choice==1){  
+			System.out.println("Enter the elements of  Linked List"); 
+			int element=ob.nextInt(); 
+			original_list.add(element);  
+			} 
+			else if(choice ==2){  
+				flag=1;  
+
+			}
+		} 
+		//segregate the original list into odd and even list. 
+		int llsize= original_list.size(); 
+		for(int i=0;i<llsize;i++){  
+			int element=original_list.get(i); 
+			if(element%2==0){  
+				even_list.add(element);
+			} 
+			else {  
+				odd_list.add(element);
+			}
+		} 
+		final_list.addAll(even_list);
+		final_list.addAll(odd_list); 
+		System.out.println("*** Final Linked List is ****"); 
+		System.out.println(final_list); 
+	}
+	
+}
--- linklist1.java
+++ linklist1.java
@@ -1,6 +1,28 @@
-class linklist1  { 
+import java.util.*;
+class Linklist1  { 
 	public static void main(String[] args) {
-		System.out.println("Just to check git hub repo");
+		Scanner ob= new Scanner(System.in); 
+		int test = ob.nextInt();
+		while(test>0){  
+			LinkedList<Integer> ll = new LinkedList<Integer>(); 
+			int n=ob.nextInt();    
+			int posi=0;
+			if(n%2==0){
+			 posi=n/2; 
+			} 
+			else {
+			 posi = (n+1)/2;  
+		}
+			while(n>0){  
+				int ele=ob.nextInt();
+				ll.add(ele);
+				n--; 
+			}  
+			int ele_add = ob.nextInt();
+			ll.add(posi,ele_add);  
+			System.out.println(ll); 
+			test--;
+		}
 	}
 	
 }
