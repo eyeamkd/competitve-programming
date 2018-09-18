c4feb98ec6b585205792fde7c46c35dc7edb53b3
Kunal Dubey <kunaldubey2297@gmail.com>
2018-09-19 02:36:49 +0530

    reversing ll

--- reversell.java
+++ reversell.java
@@ -1,23 +1,31 @@
 import java.util.*;
 import java.io.*;
-class reversell { 
-	public static void main(String[] args) {  
-		Scanner ob =new Scanner(System.in);
-		LinkedList<Integer> mylist=new LinkedList<Integer>();    
-		System.out.println("Enter the size of the linked list"); 
-		int size=ob.nextInt();
-		while(size>0){  
-			System.out.println("Enter the element"); 
-			int ele=ob.nextInt();
-			mylist.add(ele); 
-			size--;
-		}  
-		int n=mylist.size(); 
-		for(int i=0;i<n;i++) { 
-			//System.out.println("in the for loop");
-			System.out.println(""+mylist.get(n-i-1));
+
+class Reversell {   
+	Node head;
+	class Node{  
+		int data;  
+		Node next; 
+		void insert(int d){  
+			data=d; 
+			next=null; 
 		}
-		
 	}
-	
+	public static void main(String[] args) {
+		 //reversing process    
+		Node curr = new Node(); 
+		Node next = new Node(); 
+		Node prev = new Node();  
+		curr=head;  
+		prev=null;
+		while(curr!=null){   
+			next=curr.next;  
+			curr.next=prev; 
+			prev=curr;  
+		 curr=next;
+
+
+		}
+
+	}
 }
